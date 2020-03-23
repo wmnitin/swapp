ae1b3f585d87a259065dbfa935f037aa8c443390
Nitin Kumar <wmnitin@gmail.com>
2020-03-23 11:05:12 +0530

    build enabled

---
+++ build/precache-manifest.51bc38a014cbb2bb67ec1a655800cf74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478acf8443bacb993b32ad902c161cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91b991285530682a6d75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8458eefb9abe16daf597",
+    "url": "/static/js/2.e7bf0098.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7bf0098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b991285530682a6d75",
+    "url": "/static/js/main.984b0865.chunk.js"
+  },
+  {
+    "revision": "66cf7ac4888474ba63ec",
+    "url": "/static/js/runtime-main.efde7443.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
