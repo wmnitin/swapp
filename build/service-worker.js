fa38f688c4e1955b69bc25bc93ea15038eaae139
Nitin Kumar <wmnitin@gmail.com>
2020-03-23 10:22:46 +0530

    make it better

--- build/service-worker.js
+++ build/service-worker.js
@@ -33,6 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+workbox.precaching.cleanupOutdatedCaches();
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
--- src/App.js
+++ src/App.js
@@ -6,18 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>P4B OFFLINE CACHING</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
